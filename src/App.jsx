d0fc4d07c4962f398cba5042f9362e41b9a108b2
Vishal <22k20.vishal@sjec.ac.in>
2025-06-23 14:37:46 +0530

    main front page

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import gsap from 'gsap';
+import { ScrollTrigger,SplitText } from 'gsap/all';
+gsap.registerPlugin(ScrollTrigger,SplitText);
+import Navbar from './components/Navbar';
+import Hero from './components/Hero';
+const App = () => {
+  return (
+    <main>
+      <Navbar/>
+      <Hero/>
+      <div className='h-dvh bg-black'/>
+
+
+    </main>
+  )
+}
+
+export default App;

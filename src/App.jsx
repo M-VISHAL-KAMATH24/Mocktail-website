f4c046cf2ed2d0d8ce6affb2f4d43f427846f9c2
Vishal <22k20.vishal@sjec.ac.in>
2025-06-23 15:41:59 +0530

    added an cocktail and moctail details

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,18 @@ import gsap from 'gsap';
 import { ScrollTrigger,SplitText } from 'gsap/all';
 gsap.registerPlugin(ScrollTrigger,SplitText);
 import Navbar from './components/Navbar';
+import Cocktails from './components/Cocktails';
+import About from './components/About';
 import Hero from './components/Hero';
 const App = () => {
   return (
     <main>
       <Navbar/>
       <Hero/>
-      
+      <Cocktails/>
+      <About/>
+
+
 
 
     </main>
